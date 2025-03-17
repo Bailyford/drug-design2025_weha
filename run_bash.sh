0c3e744e02dc25023fc94998f07ce36bbe65bbaa
Bailyford <125068028+Bailyford@users.noreply.github.com>
2025-03-17 16:19:49 -0400

    Update run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -4,14 +4,14 @@ cd ~/
 ln -s $TMPDIR
 
 # Create and activate virtualenv
-python -m venv drug-design2025 --system-site-packages
-source ~/drug-design2025/bin/activate
-python -m ipykernel install --user --name=drug-design2025
+python -m venv drug-design2025_weha --system-site-packages
+source ~/drug-design2025_weha/bin/activate
+python -m ipykernel install --user --name=drug-design2025_weha
 
 # Clone WESTPA workshop github repo and install everything into virtualenv
 cd $TMPDIR
-git clone https://github.com/jeremyleung521/drug-design2025
-cd drug-design2025
+git clone https://github.com/Bailyford/drug-design2025_weha
+cd drug-design2025_weha
 python -m pip install -U -r requirements.txt
 cd alanine-dipeptide
 echo "Current working directory is $PWD"
