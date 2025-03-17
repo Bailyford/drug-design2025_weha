cce186dd44ca20fcfffe3695fac245aa51adfe5b
Bailyford <125068028+Bailyford@users.noreply.github.com>
2025-03-17 16:18:15 -0400

    Update revert_run_bash.sh

--- revert_run_bash.sh
+++ revert_run_bash.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 # Remove Kernel
-jupyter kernelspec uninstall -y drug-design2025
+jupyter kernelspec uninstall -y drug-design2025_weha
 unlink ~/$(basename $TMPDIR)
-rm -rf ~/drug-design2025
+rm -rf ~/drug-design2025_weha
 deactivate
 #python -m pip cache purge
 
